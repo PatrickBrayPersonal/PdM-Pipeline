ec1bb95448bcec5caf18461bd06fa3823cdc6ee3
Patrick Bray <patrickbray17@gmail.com>
2023-01-16 13:27:59 -0500

    working named pipeline

--- pdm/__main__.py
+++ pdm/__main__.py
@@ -5,14 +5,17 @@ This is where you import the pipeline function from its module and resolve it.
 """
 # Sematic
 from pdm.pipeline import pipeline
+from sematic import SilentResolver
 
 
 def main():
     """
     Entry point of my pipeline.
     """
-    pipeline().resolve()
+    pipeline().set(name="PdM Development", tags=["DEBUG"]).resolve()
 
 
 if __name__ == "__main__":
     main()
+
+# TODO: Build your CLI here, google CLI packages
--- pdm/pipeline.py
+++ pdm/pipeline.py
@@ -5,12 +5,66 @@ Feel free to break these definitions into as many files as you want for your
 preferred code structure.
 """
 # Sematic
+from dataclasses import dataclass
 import sematic
+import pandas as pd
+from pathlib import Path
+from sklearn.base import BaseEstimator
 
 
-@sematic.func
-def pipeline():
+@dataclass
+class PipelineOutput:
+    len_train_df: int
+    len_test_df: int
+    author: str = "Patrick Bray"
+
+
+@dataclass
+class TrainConfig:
+    learning_rate: float = 1
+    epochs: int = 14
+
+
+@sematic.func(inline=True)
+def load_pdm_dataset(train: bool, path: Path = Path("data/raw")) -> pd.DataFrame:
+    if train:
+        df = pd.read_csv(path / "PdM_train.csv")
+    else:
+        df = pd.read_csv(path / "PdM_test.csv")
+    return df
+
+
+@sematic.func(inline=False)
+def train_model(
+    config: TrainConfig,
+    train_df: pd.DataFrame,
+) -> BaseEstimator:
+    print("dev")
+
+
+@sematic.func(inline=True)
+def train_eval(
+    train_df: pd.DataFrame, test_df: pd.DataFrame, train_config: TrainConfig
+):
+    model = train_model(config=train_config, train_df=train_df)
+
+    evaluation_results = evaluate_model(
+        model=model, test_loader=test_dataloader, device=device
+    )
+    return evaluation_results
+
+
+@sematic.func(inline=True)
+def pipeline() -> PipelineOutput:
     """
     The root function of the pipeline.
     """
-    return
+    train_df = load_pdm_dataset(train=True)
+
+    test_df = load_pdm_dataset(train=False)
+    return make_output(train_df=train_df, test_df=test_df)
+
+
+@sematic.func(inline=True)
+def make_output(train_df: pd.DataFrame, test_df: pd.DataFrame) -> PipelineOutput:
+    return PipelineOutput(len_train_df=len(train_df), len_test_df=len(test_df))
