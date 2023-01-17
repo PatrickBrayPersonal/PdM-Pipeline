1bbde3cf846bb72723e9b8a70da2e0ec0251479d
Bray, Patrick [USA] <Bray_Patrick@bah.com>
2023-01-17 11:27:43 -0500

    remove sematic straggler

--- pdm/__main__.py
+++ pdm/__main__.py
@@ -4,20 +4,14 @@ This is the entry point of your pipeline.
 This is where you import the pipeline function from its module and resolve it.
 """
 import fire
-from sematic import SilentResolver
 
 from pdm.pipeline import pipeline
 
 
-def main(config: str, silent: bool = False):
+def main(config: str):
     """
     Entry point of my pipeline.
     """
-    assert isinstance(silent, bool), "silent entry must be 'True' or 'False'"
-    if silent:
-        resolver = SilentResolver()
-    else:
-        resolver = None
     pipeline(config)
 
 
